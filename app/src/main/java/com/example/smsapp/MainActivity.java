1dc67b0b2358da1949565f552ac426c314b7f067
ghalibse <ghalibse@gmail.com>
2016-08-09 12:00:33 -0400

    First Commit

---
+++ app/src/main/java/com/example/smsapp/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.smsapp;
+
+import android.os.Bundle;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.telephony.SmsManager;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button buttonSend;
+    EditText textPhoneNo;
+    EditText textSMS;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        buttonSend = (Button) findViewById(R.id.buttonSend);
+        textPhoneNo = (EditText) findViewById(R.id.editTextPhoneNo);
+        textSMS = (EditText) findViewById(R.id.editTextSMS);
+
+        buttonSend.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                String phoneNo = textPhoneNo.getText().toString();
+                String sms = textSMS.getText().toString();
+
+                try {
+                    SmsManager smsManager = SmsManager.getDefault();
+                    smsManager.sendTextMessage(phoneNo, null, sms, null, null);
+
+                    Snackbar.make(v,"Message Sent Successfully",Snackbar.LENGTH_LONG).show();
+                } catch (Exception e) {
+                    Snackbar.make(v,"Message Failed",Snackbar.LENGTH_LONG).show();
+                    e.printStackTrace();
+                }
+
+            }
+        });
+    }
+}
